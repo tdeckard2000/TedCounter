42d15c5f236ef78ab25aafc496ffd86f47f38713
Trent Deckard <tdecakrd2000@yahoo.com>
2020-05-31 20:45:21 -0600

    Link to mLab Mongo Database + Add Signup Link

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+
+const foodItemSchema = new mongoose.Schema({
+  "name": String,
+  "calories": Number,
+  "protein": Number,
+  "carbs": Number,
+  "sugars": Number,
+  "sodium": Number
+});
+
+const foodItem = mongoose.model('foodItem', foodItemSchema);
+
+// const item = new foodItem({
+//   "name": 'Apple',
+//   "calories": 80,
+//   "protein": 1,
+//   "carbs": 5,
+//   "sugars": 8,
+//   "sodium": 0
+// })
+
+// item.save((err, doc)=>{
+//   console.log(err);
+//   console.log(doc);
+// })
+
+foodItem.find({},(err, doc)=>{
+  console.log(doc);
+  console.log(err);
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -14,5 +48,6 @@ if (port == null || port == "") {
     console.log('Listening on Port 3000') 
   });
 }else{
-  app.listen(port);  
+  app.listen(port);
+  console.log('Listening on Port 5000') 
 }

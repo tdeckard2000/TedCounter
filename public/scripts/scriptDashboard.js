0c9fea480a289e331677644727d51a16fff6fc3f
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2020-10-22 20:13:23 -0600

    Rename Dashboard Script

--- public/scripts/scriptDashboard.js
+++ public/scripts/scriptDashboard.js
@@ -1,14 +1,15 @@
-$('.saveMealDiv input').on('propertychange input', (doc)=>{
-    let input = doc.target.value;
-    let className = doc.currentTarget.attributes[0].textContent;
-    className = className.replace(' ', '.');
-    for(let i=0; i < input.length; i++){
-        if (!$.isNumeric(input[i])){
-            input = input.replace(input[i],'');
-            $('.' + className).val(input);
-        }
-    }
-});
+//Not Needed?
+// $('.saveMealDiv input').on('propertychange input', (doc)=>{
+//     let input = doc.target.value;
+//     let className = doc.currentTarget.attributes[0].textContent;
+//     className = className.replace(' ', '.');
+//     for(let i=0; i < input.length; i++){
+//         if (!$.isNumeric(input[i])){
+//             input = input.replace(input[i],'');
+//             $('.' + className).val(input);
+//         }
+//     }
+// });
 
 //Filter list items from meal selector modal based on text input.
 $('#foodItemFilter').on('keyup', (doc)=>{

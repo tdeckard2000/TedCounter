d5697834beff4a551ab672ee64b69726921fbcac
Trent <tdeckard2000@yahoo.com>
2020-11-28 00:53:52 -0800

    Show name of selected item in edit modal

--- public/scripts/scriptEditItems.js
+++ public/scripts/scriptEditItems.js
@@ -1,3 +1,18 @@
 //Edit Items Script
 
-// $("a").removeClass("eachItemFormat")
+// Event Handlers ===============================
+
+//Get Selected Item Info
+$(".singleItem").on("click", (data)=>{
+    let selectionName = data.currentTarget.innerHTML;
+    let selectionId = data.currentTarget.dataset.itemid;
+    setModalTitle(selectionName);
+    console.log(selectionName + " & " + selectionId)
+})
+
+// Functions ===================================
+
+//Set Item Name in Edit Modal
+const setModalTitle = function(itemName){
+    $("#nameOfItem").html(itemName)
+}

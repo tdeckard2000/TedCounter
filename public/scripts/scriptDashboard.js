c83cf7469bc327adce4f15173546dac0dd61c991
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2020-12-16 17:15:36 -0700

    Disable next button if duplicate defaults selected. Show warning message.

--- public/scripts/scriptDashboard.js
+++ public/scripts/scriptDashboard.js
@@ -7,8 +7,9 @@ const nutritionOptions = ["Caffeine", "Calcium", "Calories", "Carbs", "Chloride"
 "Saturated Fat", "Selenium", "Sodium", "Sugar", "Trans Fat", "Threonine", "Tryptophan", "Valine", "Vitamin A", "Vitamin B1", "Vitamin B2", "Vitamin B3",
 "Vitamin B5", "Vitamin B6", "Vitamin B7", "Vitamin B9", "Vitamin B12", "Vitamin C", "Vitamin D2", "Vitamin D3", "Vitamin E", "Vitamin K", "Zinc"]
 
-//Store user nutrition info in window from DOM when page loads
+//When page loads
 $(window).on("load",()=>{
+    //Store user nutrition info in window from DOM 
     userPreferences = $("#goals").data().userpreferences;
 })
 
@@ -28,19 +29,26 @@ const setTopFourDropdownOptions = function (){
 }
 
 //Check for match in given array
-const checkForMatch = function(array){
-    for(i=0; i<array.length; i++){
-        let compare = array[i]
-        for(e=1; e<array.length; e++){
-            if(compare == array[i+e]){
-                console.log(compare + array[i+e])
-                return (true);
-            }
+const duplicateExists = function(array){
+    let item = ""
+    while(array.length > 1){
+        //Remove last array item and store in item variable
+        item = array.pop()
+        if($.inArray(item, array) !== -1){
+            return (true)
         }
-        console.log("no match")
-        return(false);
     }
+    //if while loop completes, no matches were found
+    return(false)
 }
+
+//Initialize bootstrap duplicate items selected warning toast
+// const initializeDefaultsWarningToast = function(){
+//     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+//     var toastList = toastElList.map(function (toastEl) {
+//       return new bootstrap.Toast(toastEl)
+//     })
+// }
 
 //######################## Event Listeners ########################
 
@@ -67,28 +75,35 @@ $('#foodItemFilter').on('keyup', (doc)=>{
 //Quick Add: Tapping 'more options' changes text to 'fewer options'
 $(".moreOptionsButton").on("click",()=>{
     if($(".moreOptionsButton").text() == "more options"){
-        $(".moreOptionsButton").text("fewer options")
+        $(".moreOptionsButton").text("fewer options");
     }else{
-        $(".moreOptionsButton").text("more options")
+        $(".moreOptionsButton").text("more options");
     }
 });
 
-//Defaults Modal: Display if no Top Four data exists
+//Display defaults modal if no Top Four data exists
 $(window).on("load", ()=>{
     if(userPreferences.nutritionTopFour.length < 1){
         $("#userPreferencesModal").modal("toggle");
     }
 
-    $(".modal-footer .back").prop("disabled", true)
+    $("#userBackButton").prop("disabled", true)
     setTopFourDropdownOptions();
 });
 
-//Defaults Modal: Disable Next button if dropdown selections match
+//Defaults Modal: Disable Next button if topFour dropdown selections match
 $(".topFourSelection").on("click", (data)=>{
     let selections = [];
-    $(".topFourSelection").each(function(index, obj){
+    $(".topFourSelection").each(function(){
        selections.push($(this).val())
     })
     
-    checkForMatch(selections);
+    if(duplicateExists(selections) === true){
+        $("#defaultsNextButton").prop("disabled", true);
+        $(".duplicateSelectionWarning").removeClass('hidden');
+    }else{
+        $("#defaultsNextButton").prop("disabled", false);
+        $(".duplicateSelectionWarning").addClass('hidden');
+
+    }
 })
